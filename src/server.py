232c062f9c0bb613e5a98ee2ed24ef6b15e92a7c
Pershin Maxim <m.e.pershin@tinkoff.ru>
2022-05-05 22:17:23 +0300

    update server

--- src/server.py
+++ src/server.py
@@ -1,9 +1,11 @@
+import grpc
 from . import service_pb2 as pb2
 from . import service_pb2_grpc as pb2_grpc
 
 
 class Server(pb2_grpc.ServiceServicer):
 
-    def Process(self, request: pb2.ProcessRequest, context) -> pb2.ProcessResponse:
-        print(type(context))
+    def Process(self, request: pb2.ProcessRequest,
+                      context: grpc.ServicerContext) -> pb2.ProcessResponse:
+        # TODO: add logic
         return pb2.ProcessResponse()
